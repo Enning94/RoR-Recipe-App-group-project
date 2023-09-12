388dc79c7a3779d353aced3640c931249a369b79
Enning94 <enningfestus@gmail.com>
2023-09-12 15:59:14 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root 'foods#index'
+
   resources :foods
   resources :recipe_foods
   resources :users
   resources :recipes
+  resources :public_recipes, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
