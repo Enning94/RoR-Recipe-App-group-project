887e36b05ff396d1be70f7a947df093fe3c95df8
Enning94 <enningfestus@gmail.com>
2023-09-12 16:27:46 +0000

    check linters

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   resources :users
   resources :recipes
   resources :public_recipes, only: %i[index show]
+
+  # resources :recipes, only: [:index, :show, :new, :update, :destroy, :create] do
+  #   resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
+  # end
+
+  # resources :foods do
+  #   resources :recipe_foods, only: %i[edit, update, new create destroy]
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
